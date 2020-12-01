87084018da0897ace9b663f400e7eb2dee65cb69
Rico Hengst <rico.hengst@tropos.de>
2020-12-01 11:16:04 +0100

    Improve conf.py to use a defined master_doc file

--- source/conf.py
+++ source/conf.py
@@ -51,6 +51,9 @@ extensions = [
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
+# Set master (rst file)
+master_doc = 'index'
+
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
@@ -81,3 +84,20 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+
+# -- Options for LaTeX output ------------------------------------------------
+
+latex_elements = {
+    # The paper size ('letterpaper' or 'a4paper').
+    #
+    'papersize': 'a4paper',
+
+    # The font size ('10pt', '11pt' or '12pt').
+    #
+    # 'pointsize': '10pt',
+
+    # Additional stuff for the LaTeX preamble.
+    #
+    'preamble': r'\usepackage{pmboxdraw} \usepackage[utf8x]{inputenc} ',
+}
